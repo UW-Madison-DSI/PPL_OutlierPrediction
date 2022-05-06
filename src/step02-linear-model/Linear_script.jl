8eb34392e9a53489dcd8df96dc2893fb733fb277
MarlinRLee <Marlinrlee0@gmail.com>
2022-05-06 13:22:31 -0500

    updated step 2 with log linear side by side

--- src/step02-linear-model/Linear_script.jl
+++ src/step02-linear-model/Linear_script.jl
@@ -1,13 +1,26 @@
-function visualize_trace(trace::Trace; title="", YscaleFunc::Function =  identity)
+function log_modulus(x)
+    return sign(x)*log1p(abs(x))
+end
+
+function visualize_trace(trace::Trace; title="")
     trace = serialize_trace(trace)
     #Graph points
     outliers = [pt for (pt, outlier) in zip(trace[:points], trace[:outliers]) if outlier]
     inliers =  [pt for (pt, outlier) in zip(trace[:points], trace[:outliers]) if !outlier]
-    PLT = Plots.scatter(map(first, inliers), YscaleFunc.(map(last, inliers)), markercolor="blue", label=nothing, title=title) 
-    PLT = Plots.scatter!(map(first, outliers), YscaleFunc.(map(last, outliers)), markercolor="red", label=nothing)
+    PLT = Plots.scatter(map(first, inliers), map(last, inliers), markercolor="blue", label=nothing) 
+    PLT = Plots.scatter!(map(first, outliers), map(last, outliers), markercolor="red", label=nothing)
+
     #Graph Line
-    PLT = Plots.plot!(trace[:xs], YscaleFunc.(trace[:ys]), color = "black", lw = 3, label = nothing)
-    return PLT
+    PLT = Plots.plot!(trace[:xs], trace[:ys], color = "black", lw = 3, label = nothing)
+
+
+    #LogPlot
+    ExpPLT = Plots.scatter(map(first, inliers), log_modulus.(map(last, inliers)), markercolor="blue", label=nothing) 
+    ExpPLT = Plots.scatter!(map(first, outliers), log_modulus.(map(last, outliers)), markercolor="red", label=nothing)
+    ExpPLT = Plots.plot!(trace[:xs], log_modulus.(trace[:ys]), color = "black", lw = 3, label = nothing)
+    
+    DuoPlot = plot(PLT,ExpPLT,title=title)
+    return DuoPlot
 end
 
 function make_constraints(ys::Vector{Float64})
