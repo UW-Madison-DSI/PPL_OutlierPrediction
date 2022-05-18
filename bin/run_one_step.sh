d6acf43c6261e92cf56b41bc0b3076985b1b7cb3
Steve Goldstein <sgoldstein@wisc.edu>
2022-05-18 17:27:37 -0500

    ready to test submitting

--- bin/run_one_step.sh
+++ bin/run_one_step.sh
@@ -6,31 +6,39 @@
 #
 #####################################
 # parse cmd line args
-tarFile=$1;  shift
-juliaScript=$1; shift
-juliaScriptDir=$1; shift
-gitHubRepo=$1; shift
+juliaTarFile=$1;  shift
+repoZipFile=$1; shift
+step=$1; shift
+CLUSTER=$1; shift
+PROCESS=$1; shift
 #############
 #  Set up remote environment
-tar zxf $tarFile
-tar zxf $gitHubRepo
 
-
-juliaDir=${tarFile/-linux*/}
+## Julia
+tar zxf $juliaTarFile
+juliaDir=${jualiaTarFile/-linux*/}
 
 export PATH=$HOME/$juliaDir/bin:$PATH
 export JULIA_DEPOT_PATH=$HOME/$juliaDir/site_packages:$JULIA_DEPOT_PATH
 
+### PPL code
+unzip -qq $repoZipFile
+pushd PPL_OutlierPrediction*/src/$step-*/
+
 ####################
 #  Run the script
+julia $step.jl
+status=$?
 
-pushd */src/$juliaScriptDir
-julia $juliaScript
+## missing:  validate that toml env is consistent with squid tarball;
+## julia script to update or check the env??
+
+## copy image file to top level dir so that it will be transferred
+##  rename with job ID to prevent clobbering 
+cp $step.png $HOME/$step.$CLUSTER.$PROCESS.png  
 popd
 
 ##################
-# check return codes for errors and exit
-status=$?
-
+# exit with return code of julia execution.
 exit($status)
 
